ad8a9cdb44b49ab9d19d0ad692ac8ecc6d91d0c8
nmemarcoding <nmemarcoding@gmail.com>
2023-05-24 21:54:39 -0700

    get products based on search query

--- client/src/pages/ProductListPage/ProductListPage.js
+++ client/src/pages/ProductListPage/ProductListPage.js
@@ -34,61 +34,63 @@ export default function ProductListPage() {
 
   return (
     <>
-    <Navbar />
-    <div className="max-w-screen-xl mx-auto p-4 pt-32">
-      <div className="flex flex-wrap justify-between mb-4">
-        <input
-          type="text"
-          placeholder="Filter by name"
-          value={nameFilter}
-          onChange={(e) => setNameFilter(e.target.value)}
-          className="w-full md:w-auto md:mr-4 mb-4 md:mb-0 px-4 py-2 border border-gray-300 rounded-md"
-        />
-        <input
-          type="text"
-          placeholder="Filter by price"
-          value={priceFilter}
-          onChange={(e) => setPriceFilter(e.target.value)}
-          className="w-full md:w-auto px-4 py-2 border border-gray-300 rounded-md"
-        />
-        <div className="relative">
-          <select
-            value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value)}
-            className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-md shadow-md focus:outline-none focus:shadow-outline-blue focus:border-blue-500"
-          >
-            <option value="asc">Price: Low to High</option>
-            <option value="desc">Price: High to Low</option>
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-              <path
-                d="M14.707 7.293a1 1 0 0 0-1.414 0L10 10.586l-3.293-3.293a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0 0-1.414z"
-              />
-            </svg>
+      <Navbar />
+      <div className="max-w-screen-xl mx-auto p-4 bg-gray-100 rounded-lg shadow-lg">
+        <div className="flex flex-wrap justify-between mb-4">
+          <input
+            type="text"
+            placeholder="Filter by name"
+            value={nameFilter}
+            onChange={(e) => setNameFilter(e.target.value)}
+            className="w-full md:w-auto md:mr-4 mb-4 md:mb-0 px-4 py-2 border border-gray-300 rounded-md shadow-md"
+          />
+
+          <input
+            type="text"
+            placeholder="Filter by price"
+            value={priceFilter}
+            onChange={(e) => setPriceFilter(e.target.value)}
+            className="w-full md:w-auto px-4 py-2 border border-gray-300 rounded-md mb-4 md:mb-0 shadow-md"
+          />
+
+          <div className="relative">
+            <select
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+              className="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-500 px-4 py-2 pr-8 rounded-md shadow-md focus:outline-none focus:shadow-outline-blue focus:border-blue-500"
+            >
+              <option value="asc">Price: Low to High</option>
+              <option value="desc">Price: High to Low</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path
+                  d="M14.707 7.293a1 1 0 0 0-1.414 0L10 10.586l-3.293-3.293a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0 0-1.414z"
+                />
+              </svg>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {sortedProducts.map((product) => (
-          <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-            <div className="p-4">
-              <img src={product.image} alt={product.name} className="w-full mb-4" />
-              <div className="font-bold text-lg mb-2">{product.name}</div>
-              <div className="text-gray-700 text-base">${product.price}</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {sortedProducts.map((product) => (
+            <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden transform hover:-translate-y-1 transition-all duration-300">
+              <div className="p-4">
+                <img src={product.image} alt={product.name} className="w-full mb-4 rounded-md shadow-md" />
+                <div className="font-bold text-lg mb-2">{product.name}</div>
+                <div className="text-gray-700 text-base">${product.price}</div>
+              </div>
+              <div className="p-4">
+                <button
+                  onClick={() => addToCart(product)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md shadow-md"
+                >
+                  Add to cart
+                </button>
+              </div>
             </div>
-            <div className="p-4">
-              <button
-                onClick={() => addToCart(product)}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 }
