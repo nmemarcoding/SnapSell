8ae6ab5afbf1fff0e7aad95c1840b1539c88d4b3
nmemarcoding <nmemarcoding@gmail.com>
2023-05-24 23:30:38 -0700

    Link for home page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -44,14 +44,15 @@ export default function Navbar() {
       <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
         
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
+          
+          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
+          <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
-          </a>
+          </Link>
         </div>
         <div className="hidden lg:block mr-4">
           <div className="relative rounded-md shadow-sm">
