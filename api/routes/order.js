c5aa7dde0cf99d1135284f3c8bd377bb5141b9d1
nmemarcoding <nmemarcoding@gmail.com>
2023-05-31 13:11:33 -0700

    route to get order details base on order number

--- api/routes/order.js
+++ api/routes/order.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const Product = require('../models/product.js');
-const user = require('../models/user.js');
-const cart = require('../models/cart.js');
-const order = require('../models/order.js');
+const User = require('../models/user.js');
+const Cart = require('../models/cart.js');
+const Order = require('../models/order.js');
+const { default: mongoose } = require('mongoose');
 
 // creat order route
 router.post('/', async (req, res) => {
     try {
         // Finding user cart by user id
-        const userCart = await cart.findOne({
+        const userCart = await Cart.findOne({
             user: req.body.userId
         }).populate("items.product");
       
@@ -26,7 +27,7 @@ router.post('/', async (req, res) => {
         });
 
         // Adding shipping address to the order and also total price from the cart
-        const newOrder = new order({
+        const newOrder = new Order({
             user: req.body.userId,
             items: orderItems,
             shippingAddress: req.body.shippingAddress,
@@ -37,7 +38,7 @@ router.post('/', async (req, res) => {
         const savedOrder = await newOrder.save();
 
         // Deleting the user's cart after order is saved
-        await cart.deleteOne({ user: req.body.userId });
+        await Cart.deleteOne({ user: req.body.userId });
 
         res.status(201).json(savedOrder);
     } catch (error) {
@@ -45,7 +46,25 @@ router.post('/', async (req, res) => {
         return res.status(500).json("Internal Server Error");
     }
 });
-
+//  getiing order base on order number
+router.get('/:orderNumber', async (req, res) => {
+    
+    try {
+        // cheack if order number is valid
+        if (!mongoose.Types.ObjectId.isValid(req.params.orderNumber)) {
+            return res.status(400).json('Invalid order number');
+        }
+      const order = await Order.findOne({ _id: req.params.orderNumber })
+        .populate('items.product');
+      if (!order) {
+        return res.status(404).json('Order not found');
+      }
+      res.json(order);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('Internal Server Error');
+    }
+  });
     
 
 
