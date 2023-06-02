b6a91cecad270c04bba0aacc6e28fa917006300f
nmemarcoding <nmemarcoding@gmail.com>
2023-06-02 11:08:04 -0700

    card with next and pick btn to look at order item and pick them

--- client/src/pages/AdminOrderProcessingPage/AdminOrderProcessingPage.js
+++ client/src/pages/AdminOrderProcessingPage/AdminOrderProcessingPage.js
@@ -5,73 +5,83 @@ import { publicRequest } from '../../hooks/requestMethods';
 export default function AdminOrderProcessingPage() {
   const [items, setItems] = useState([]);
   const [currentItemIndex, setCurrentItemIndex] = useState(0);
+  const [address, setAddress] = useState('');
 
   useEffect(() => {
-    // Make an API call to fetch the cart items from the backend
-    // geting name query from url
-    const orderId = window.location.href.split("?")[1].split("=")[1];
-
+    const orderId = window.location.href.split('?')[1].split('=')[1];
     publicRequest()
-        
       .get(`order/${orderId}`)
       .then((response) => {
-        
-        setItems(response.data.items); // Set the items state to the response data
+        setItems(response.data.items);
+        setAddress(response.data.shippingAddress);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-  const handleItemReady = (itemId) => {
-    setItems((prevItems) =>
-      prevItems.map((item) =>
-        item._id === itemId ? { ...item, readyForShipping: true } : item
-      )
-    );
-    setCurrentItemIndex((prevIndex) => prevIndex + 1);
+  const nextItem = () => {
+    if (currentItemIndex >= items.length - 1) {
+      setCurrentItemIndex(0);
+    } else {
+      setCurrentItemIndex((prevIndex) => prevIndex + 1);
+    }
   };
 
-  const currentItem = items[currentItemIndex]?.product;
+  const pickItem = () => {
+    // remove item from items array with current index
+    const newItems = [...items];
+    newItems.splice(currentItemIndex, 1);
+    setItems(newItems);
+  };
+
+
 
   return (
-    <>
-      <AdminNavbar />
-      <div className="flex flex-col items-center justify-center h-screen">
+    <div className="bg-gray-100 min-h-screen">
+      <AdminNavbar /> 
+      <br></br>
+      <div className="flex flex-col items-center justify-center h-full">
         <h1 className="text-3xl font-bold mb-8">Order Processing</h1>
-        {currentItem ? (
-          <div className="flex flex-col items-center">
-            <h2 className="text-2xl font-bold mb-4">
-              Current Item: {currentItem.title}
-            </h2>
-            <div className="mb-4">
-              <img
-                src={currentItem.img}
-                alt={`Image of ${currentItem.title}`}
-              />
-            </div>
-            <p className="text-lg font-bold mb-2">
-              Quantity: {items[currentItemIndex].quantity}
-            </p>
-            {items[currentItemIndex].readyForShipping ? (
-              <p className="text-green-500 font-bold">
-                Item is ready for shipping
+        <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
+          {items.length === 0 ? (
+            <>
+              <h2 className="text-2xl font-bold mb-4">Done</h2>
+              <p className="text-lg font-bold mb-2">Shipping Address: {address}</p>
+            </>
+          ) : (
+            <>
+              <h2 className="text-2xl font-bold mb-4">
+                Current Item: {items[currentItemIndex]?.product?.title}
+              </h2>
+              <div className="mb-4">
+                <img
+                  src={items[currentItemIndex]?.product?.img}
+                  alt={`Image of ${items[currentItemIndex]?.product?.title}`}
+                  className="w-64 h-64 object-contain"
+                />
+              </div>
+              <p className="text-lg font-bold mb-2">
+                Quantity: {items[currentItemIndex]?.quantity}
               </p>
-            ) : (
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={() => handleItemReady(items[currentItemIndex]._id)}
-              >
-                Mark as Ready
-              </button>
-            )}
-          </div>
-        ) : (
-          <p className="text-green-500 font-bold">
-            All items have been processed
-          </p>
-        )}
+              <div className="flex">
+                <button
+                  onClick={pickItem}
+                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
+                >
+                  Pick Item
+                </button>
+                <button
+                  onClick={nextItem}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Next
+                </button>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
