7ce10a2e47819f9ceb47455fafa7cf1697ee6da3
nmemarcoding <nmemarcoding@gmail.com>
2023-06-02 12:10:39 -0700

    updating oprder status

--- client/src/pages/AdminOrderProcessingPage/AdminOrderProcessingPage.js
+++ client/src/pages/AdminOrderProcessingPage/AdminOrderProcessingPage.js
@@ -35,53 +35,73 @@ export default function AdminOrderProcessingPage() {
     setItems(newItems);
   };
 
+  const shipItems = () => { 
+         const orderId = window.location.href.split('?')[1].split('=')[1];
+        publicRequest()
+        .put(`order/${orderId}`,{status:"shipped"})
+        .then((response) => {
+            window.alert("Order Shipped")
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error);
+        }
+    );
+        
+    };
+
 
 
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <AdminNavbar /> 
-      <br></br>
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-3xl font-bold mb-8">Order Processing</h1>
-        <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
-          {items.length === 0 ? (
-            <>
-              <h2 className="text-2xl font-bold mb-4">Done</h2>
-              <p className="text-lg font-bold mb-2">Shipping Address: {address}</p>
-            </>
-          ) : (
-            <>
-              <h2 className="text-2xl font-bold mb-4">
-                Current Item: {items[currentItemIndex]?.product?.title}
-              </h2>
-              <div className="mb-4">
-                <img
-                  src={items[currentItemIndex]?.product?.img}
-                  alt={`Image of ${items[currentItemIndex]?.product?.title}`}
-                  className="w-64 h-64 object-contain"
-                />
-              </div>
-              <p className="text-lg font-bold mb-2">
-                Quantity: {items[currentItemIndex]?.quantity}
-              </p>
-              <div className="flex">
-                <button
-                  onClick={pickItem}
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
-                >
-                  Pick Item
-                </button>
-                <button
-                  onClick={nextItem}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                  Next
-                </button>
-              </div>
-            </>
-          )}
-        </div>
+  <div className="bg-gray-100 min-h-screen">
+    <AdminNavbar /> 
+    <br></br>
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-3xl font-bold mb-8">Order Processing</h1>
+      <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-lg">
+        {items.length === 0 ? (
+          <>
+            <h2 className="text-2xl font-bold mb-4">Done</h2>
+            <p className="text-lg font-bold mb-2">Shipping Address: {address}</p>
+            <button
+              onClick={shipItems }
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Ship Items
+            </button>
+          </>
+        ) : (
+          <>
+            <h2 className="text-2xl font-bold mb-4">
+              Current Item: {items[currentItemIndex]?.product?.title}
+            </h2>
+            <div className="mb-4">
+              <img
+                src={items[currentItemIndex]?.product?.img}
+                alt={`Image of ${items[currentItemIndex]?.product?.title}`}
+                className="w-64 h-64 object-contain"
+              />
+            </div>
+            <p className="text-lg font-bold mb-2">
+              Quantity: {items[currentItemIndex]?.quantity}
+            </p>
+            <div className="flex">
+              <button
+                onClick={pickItem}
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4"
+              >
+                Pick Item
+              </button>
+              <button
+                onClick={nextItem}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Next
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </div>
-  );
+  </div>
+);
 }
